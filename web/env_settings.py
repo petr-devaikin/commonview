3c03b2aa0511d4d04948d507a809d46e111ec0ec
Petr Devaikin <p.devaikin@gmail.com>
2015-03-19 19:09:32 +0300

    env conf

--- web/env_settings.py
+++ web/env_settings.py
@@ -3,5 +3,5 @@ from os import environ
 def load_env(app):
     if 'DATABASE_URI' in environ: app.config['DATABASE_URI'] = environ.get('DATABASE_URI')
 
-    if 'INSTA_ID' in environ: app.config['INSTA_ID'] = int(environ.get('INSTA_ID'))
+    if 'INSTA_ID' in environ: app.config['INSTA_ID'] = environ.get('INSTA_ID')
     if 'INSTA_SECRET' in environ: app.config['INSTA_SECRET'] = environ.get('INSTA_SECRET')
