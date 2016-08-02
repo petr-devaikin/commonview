34e3a335ad80257f68e34161cdb251117c32c891
Petr Devaikin <p.devaikin@gmail.com>
2016-08-02 18:31:28 +0200

    merge for better colors not tested

--- web/picprocess/image_helper.py
+++ web/picprocess/image_helper.py
@@ -2,9 +2,6 @@ from PIL import Image
 import cStringIO
 from flask import current_app
 import urllib2
-import datetime
-import os
-from web.logger import get_logger
 
 class ImageHelper:
     @staticmethod
@@ -24,13 +21,25 @@ class ImageHelper:
     def get_new_image(url):
         try:
             f = cStringIO.StringIO(urllib2.urlopen(url).read())
-            img = Image.open(f)
+            img_o = Image.open(f)
+
+            w, h = img_o.size
+            if w == h:
+                img = img_o
+            elif w > h:
+                left = (w - h) / 2
+                right = left + h
+                img = img_o.crop(box=(left, 0, right, h))
+            else:
+                top = (h - w) / 2
+                bottom = top + w
+                img = img_o.crop(box=(0, top, w, bottom))
 
             img.thumbnail((current_app.config['EXPORT_GROUP_SIZE'], current_app.config['EXPORT_GROUP_SIZE']))
-            high_pic = img.tostring('raw', 'RGB')
+            high_pic = img.tobytes('raw', 'RGB')
 
             img.thumbnail((current_app.config['GROUP_SIZE'], current_app.config['GROUP_SIZE']))
-            low_pic = img.tostring('raw', 'RGB')
+            low_pic = img.tobytes('raw', 'RGB')
 
             return high_pic, low_pic
         except urllib2.HTTPError:
@@ -38,36 +47,19 @@ class ImageHelper:
 
     @staticmethod
     def compile_image(picture):
-        file_name = picture.export_full_path()
+        export_size = current_app.config['EXPORT_GROUP_SIZE']
+        group_size = current_app.config['GROUP_SIZE']
+        width = picture.width * export_size / group_size
+        height = picture.height * export_size / group_size
 
-        if picture.export_generated == None or picture.export_generated < picture.updated:
-            get_logger().info('Generate new export for %d', picture.id)
+        img = Image.new('RGBA', (width, height))
+        for f in picture.fragments:
+            if f.x != None and f.y != None:
+                fimg = Image.frombytes('RGB', (export_size, export_size), f.high_pic, 'raw')
+                img.paste(fimg, (f.x * export_size, f.y * export_size))
 
-            export_size = current_app.config['EXPORT_GROUP_SIZE']
-            width = picture.fragment_width() * export_size
-            height = picture.fragment_height() * export_size
+        img_io = cStringIO.StringIO()
+        img.save(img_io, 'PNG', quality=70)
+        img_io.seek(0)
 
-            img = Image.new('RGBA', (width, height))
-            for f in picture.fragments:
-                if f.x != None and f.y != None:
-                    fimg = Image.fromstring('RGB', (export_size, export_size), f.high_pic, 'raw')
-                    img.paste(fimg, (f.x * export_size, f.y * export_size))
-
-            img.save(os.path.join('web', file_name), 'PNG')
-            picture.export_generated = datetime.datetime.now()
-            picture.save()
-
-        return file_name
-
-    @staticmethod
-    def mini_image(picture):
-        file_name = ImageHelper.compile_image(picture)
-
-        img = Image.open('web/' + file_name)
-        img = img.resize((picture.width * 2, picture.height * 2))
-        data = cStringIO.StringIO()
-        img.save(data, 'PNG')
-        data.seek(0)
-
-        return data
-        
+        return img_io
--- web/static/js/pixel_group.js
+++ web/static/js/pixel_group.js
@@ -1,24 +1,52 @@
-define(['settings'], function(settings) {
-    return function(data) {
-        this.x = data.x;
-        this.y = data.y;
-        this.pixels = data.colors;
-
+define(['settings', 'helpers', 'libs/deltae', 'libs/d3'], function(settings, helpers, deltae, d3) {
+    return function(x, y) {
+        this.x = x;
+        this.y = y;
+        this.pixels = new Array(3 * settings.groupSize * settings.groupSize);
         this.image = undefined;
         this.loading = false;
+        this.lab = [];
 
         this.calcDiff = function(colors) {
             var summa = 0;
             var count = 0;
-            for (var i = 0; i < colors.length; i++)
+            for (var i = 0; i < colors.length / 3; i++)
                 if (this.pixels[i] !== undefined) {
+                    var color1 = d3.lab('rgb('+colors[3 * i]+','+colors[3 * i + 1]+','+colors[3 * i + 2]+')');
+                    var color2 = this.lab[i];
+                    color1 = {
+                        L: color1.l,
+                        A: color1.a,
+                        B: color1.b,
+                    }
                     count++;
-                    summa += Math.abs(this.pixels[i] - colors[i]);
+                    summa += deltae.getDeltaE00(color1, color2);
                 }
             return summa / count;
         }
 
-        this.setImage = function(data) {
+        this.addPixel = function(dX, dY, color) {
+            this.pixels[3 * (dY * settings.groupSize + dX)] = color[0];
+            this.pixels[3 * (dY * settings.groupSize + dX) + 1] = color[1];
+            this.pixels[3 * (dY * settings.groupSize + dX) + 2] = color[2];
+            var c = d3.lab('rgb('+color[0]+','+color[1]+','+color[2]+')');
+            this.lab[dY * settings.groupSize + dX] = { L: c.l, A: c.a, B: c.b };
+        }
+
+        this.toHash = function() {
+            return null;
+            if (!this.changed)
+                return undefined;
+            else {
+                return {
+                    x: this.x,
+                    y: this.y,
+                    image: this.image.toHash(),
+                }
+            }
+        }
+
+        this.fromHash = function(data) {
             this.image = data;
         }
     }
